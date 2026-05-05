52738559d8ec18ddb96bc5288d1ac853861c4d9c
Azarias Galama <azariasabera@gmail.com>
2026-05-05 12:20:42 +0300

    Create branch release from feature and modify hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,4 @@
 #print("Hello World!") # Create by me
 print("Hello world!") # From https://course-gitlab.tuni.fi/git-course/intermediate-branches.git
 print("Hello from feature")
+print("Hello from release")
